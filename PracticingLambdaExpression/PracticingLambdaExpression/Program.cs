4e10619d1d840068df918b728415c9761c04bbda
Sharath <Sharus9587@gmail.com>
2021-05-20 15:30:34 +0530

    Added : Retrive average age in the list

--- PracticingLambdaExpression/PracticingLambdaExpression/Program.cs
+++ PracticingLambdaExpression/PracticingLambdaExpression/Program.cs
@@ -10,28 +10,29 @@ namespace PracticingLambdaExpression
         {
             List<Person> listPersonInCity = new List<Person>();
             AddRecords(listPersonInCity);
-            Retriving_TopTwoRecord_ForAgeIs_LessThanSixty(listPersonInCity);
+           // Retriving_TopTwoRecord_ForAgeIs_LessThanSixty(listPersonInCity);
             CheckingForTeenagerPerson(listPersonInCity);
-
+            AllPersonsAverageAge(listPersonInCity);
+           
         }
         private static void AddRecords(List<Person> listPersonInCity)
-        {
-            listPersonInCity.Add(new Person("203456876", "John", "12 Main Street, Newyork,NY", 15));
-            listPersonInCity.Add(new Person("203456877", "SAM", "13 Main Ct, Newyork,NY", 25));
-            listPersonInCity.Add(new Person("203456878", "Elan", "14 Main Street, Newyork,NY", 35));
-            listPersonInCity.Add(new Person("203456879", "Smith", "12 Main Street, Newyork,NY", 45));
-            listPersonInCity.Add(new Person("203456880", "SAM", "345 Main Ave, Dayton,OH", 55));
-            listPersonInCity.Add(new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork,NY", 65));
-            listPersonInCity.Add(new Person("203456882", "Winston", "1208 Alex st, Newyork,NY", 65));
-            listPersonInCity.Add(new Person("203456883", "Mac", "126 Province Ave, Baltimore,NY", 85));
-            listPersonInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore,NY", 95));
-            // Console.WriteLine(listPersonInCity.ToString());
-            // listPersonInCity.ForEach(x => Console.WriteLine("{0}\t",x.Name.ToString()));
+            {
+                listPersonInCity.Add(new Person("203456876", "John", "12 Main Street, Newyork,NY", 15));
+                listPersonInCity.Add(new Person("203456877", "SAM", "13 Main Ct, Newyork,NY", 25));
+                listPersonInCity.Add(new Person("203456878", "Elan", "14 Main Street, Newyork,NY", 35));
+                listPersonInCity.Add(new Person("203456879", "Smith", "12 Main Street, Newyork,NY", 45));
+                listPersonInCity.Add(new Person("203456880", "SAM", "345 Main Ave, Dayton,OH", 55));
+                listPersonInCity.Add(new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork,NY", 65));
+                listPersonInCity.Add(new Person("203456882", "Winston", "1208 Alex st, Newyork,NY", 65));
+                listPersonInCity.Add(new Person("203456883", "Mac", "126 Province Ave, Baltimore,NY", 85));
+                listPersonInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore,NY", 95));
+                // Console.WriteLine(listPersonInCity.ToString());
+                // listPersonInCity.ForEach(x => Console.WriteLine("{0}\t",x.Name.ToString()));
 
-        }
-        private static void Retriving_TopTwoRecord_ForAgeIs_LessThanSixty(List<Person> listPersonsInCity)
-        {
-            foreach (Person person in listPersonsInCity.FindAll(e => (e.Age < 60)).Take(2).ToList())
+            }
+            private static void Retriving_TopTwoRecord_ForAgeIs_LessThanSixty(List<Person> listPersonsInCity)
+            {
+                foreach (Person person in listPersonsInCity.FindAll(e=> (e.Age < 60)).Take(2).ToList())
             {
                 Console.WriteLine("Name : " + person.Name + "\t\tAge : " + person.Age);
             }
@@ -45,6 +46,13 @@ namespace PracticingLambdaExpression
             else
                 Console.WriteLine("NO, we dont have Teen-agers in list");
         }
+        public static void AllPersonsAverageAge(List<Person> listPersonInCity)
+        {
+            double avgAge = listPersonInCity.Average(e => e.Age);
+            Console.WriteLine("The Average of all the person's age is :" + avgAge);
+
+        }
+
+        }
     }
-}
 
